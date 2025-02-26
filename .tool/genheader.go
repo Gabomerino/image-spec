e8c996828e5012f6f936b0bd78b1513017476923
Gabomerino <190378893+Gabomerino@users.noreply.github.com>
2025-02-26 15:11:24 -0600

    Actualizar genheader.go

--- .tool/genheader.go
+++ .tool/genheader.go
@@ -17,16 +17,4 @@ func main() {
 		Branch:  specs.Version,
 	}
 	if strings.HasSuffix(specs.Version, "-dev") {
-		cmd := exec.Command("git", "log", "-1", `--pretty=%H`, "HEAD")
-		var out bytes.Buffer
-		cmd.Stdout = &out
-		cmd.Stderr = os.Stderr
-		if err := cmd.Run(); err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			os.Exit(1)
-		}
-
-		obj.Branch = strings.Trim(out.String(), " \n\r")
-	}
-	headerTemplate.Execute(os.Stdout, obj)
-}
+		cmd := exec.
